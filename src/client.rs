923326a07a463a1da33d6bb34f510acb93a06621
alonginwind <sunzl3721@gmail.com>
2026-08-20 09:25:27 +0800

    优化tcp打洞直连成功率

--- src/client.rs
+++ src/client.rs
@@ -1368,7 +1368,7 @@ impl Client {
         let mut webrtc_guard = webrtc_offerer.map(OffererGuard::new);
         let direct_failures = interface.get_lch().read().unwrap().direct_failures;
         let mut connect_timeout = 0;
-        const MIN: u64 = 1000;
+        const MIN: u64 = 2000;
         if is_local || peer_nat_type == NatType::SYMMETRIC {
             connect_timeout = MIN;
         } else {
