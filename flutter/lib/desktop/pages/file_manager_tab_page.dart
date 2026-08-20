b7157fa5e8a40168c0616fa28ca9b115034f8cb6
alonginwind <sunzl3721@gmail.com>
2026-08-20 10:46:57 +0800

    文件传输和终端添加连接类型图标

--- flutter/lib/desktop/pages/file_manager_tab_page.dart
+++ flutter/lib/desktop/pages/file_manager_tab_page.dart
@@ -3,12 +3,14 @@ import 'dart:convert';
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hbb/common.dart';
+import 'package:flutter_hbb/common/shared_state.dart';
 import 'package:flutter_hbb/common/widgets/dialog.dart';
 import 'package:flutter_hbb/consts.dart';
 import 'package:flutter_hbb/models/state_model.dart';
 import 'package:flutter_hbb/desktop/pages/file_manager_page.dart';
 import 'package:flutter_hbb/desktop/widgets/tabbar_widget.dart';
 import 'package:flutter_hbb/utils/multi_window_manager.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 import '../../models/platform_model.dart';
@@ -35,24 +37,31 @@ class _FileManagerTabPageState extends State<FileManagerTabPage> {
       WindowController.fromWindowId(windowId())
           .setTitle(getWindowNameWithId(id));
     };
-    tabController.onRemoved = (_, id) => onRemoveId(id);
+    tabController.onRemoved = (_, id) {
+      ConnectionTypeState.delete(id);
+      FingerprintState.delete(id);
+      onRemoveId(id);
+    };
+    final initialId = params['id']!;
+    ConnectionTypeState.init(initialId);
+    FingerprintState.init(initialId);
     tabController.add(TabInfo(
-        key: params['id'],
-        label: params['id'],
+        key: initialId,
+        label: initialId,
         selectedIcon: selectedIcon,
         unselectedIcon: unselectedIcon,
         onTabCloseButton: () async {
           if (await desktopTryShowTabAuditDialogCloseCancelled(
-            id: params['id'],
+            id: initialId,
             tabController: tabController,
           )) {
             return;
           }
-          tabController.closeBy(params['id']);
+          tabController.closeBy(initialId);
         },
         page: FileManagerPage(
-          key: ValueKey(params['id']),
-          id: params['id'],
+          key: ValueKey(initialId),
+          id: initialId,
           password: params['password'],
           isSharedPassword: params['isSharedPassword'],
           tabController: tabController,
@@ -72,6 +81,8 @@ class _FileManagerTabPageState extends State<FileManagerTabPage> {
       if (call.method == kWindowEventNewFileTransfer) {
         final args = jsonDecode(call.arguments);
         final id = args['id'];
+        ConnectionTypeState.init(id);
+        FingerprintState.init(id);
         windowOnTop(windowId());
         tabController.add(TabInfo(
             key: id,
@@ -117,6 +128,52 @@ class _FileManagerTabPageState extends State<FileManagerTabPage> {
           tail: const AddButton(),
           selectedBorderColor: MyTheme.accent,
           labelGetter: DesktopTab.tablabelGetter,
+          tabBuilder: (key, icon, label, themeConf) => Obx(() {
+            final connectionType = ConnectionTypeState.find(key);
+            if (!connectionType.isValid()) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  icon,
+                  label,
+                ],
+              );
+            } else {
+              bool secure =
+                  connectionType.secure.value == ConnectionType.strSecure;
+              bool direct =
+                  connectionType.direct.value == ConnectionType.strDirect;
+              String msgConn = getConnectionText(
+                  secure, direct, connectionType.stream_type.value);
+              var msgFingerprint = '${translate('Fingerprint')}:\n';
+              var fingerprint = FingerprintState.find(key).value;
+              if (fingerprint.isEmpty) {
+                fingerprint = 'N/A';
+              }
+              if (fingerprint.length > 5 * 8) {
+                var first = fingerprint.substring(0, 39);
+                var second = fingerprint.substring(40);
+                msgFingerprint += '$first\n$second';
+              } else {
+                msgFingerprint += fingerprint;
+              }
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  icon,
+                  Tooltip(
+                    message: '$msgConn\n$msgFingerprint',
+                    child: SvgPicture.asset(
+                      'assets/${connectionType.secure.value}${connectionType.direct.value}.svg',
+                      width: themeConf.iconSize,
+                      height: themeConf.iconSize,
+                    ).paddingOnly(right: 5),
+                  ),
+                  label,
+                ],
+              );
+            }
+          }),
         ));
     final tabWidget = isLinux
         ? buildVirtualWindowFrame(context, child)
